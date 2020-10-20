5c4dafd1e9e6e2e3e615d7ad4725bb4f689f2309
Jackman3323 <jhughes22@kentdenver.org>
2020-10-19 21:29:46 -0600

    Alpha Version 0.0.1: All chrome files + initial xcode project files.

---
+++ CanvasGrade++/CanvasGrade++/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CanvasGrade++
+//
+//  Created by Jack Hughes on 10/19/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
